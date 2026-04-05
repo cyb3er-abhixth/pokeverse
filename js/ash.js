11045263c682e536659688cc0234f0108649aed0
cyb3er-abhixth <abhinav13347@indianschoolrak.com>
2026-04-05 16:27:41 +0400

    js fix

--- js/ash.js
+++ js/ash.js
@@ -1,5 +1,23 @@
 window.addEventListener("DOMContentLoaded", () => {
 
+    // Menu toggle (same as zmoves)
+  const menuIcon = document.getElementById("menu-icon");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const closeBtn = document.querySelector("span.close");
+  const overlay = document.querySelector("span.overlay");
+
+  function toggleMenu() {
+    mobileMenu.classList.toggle("active");
+    closeBtn.classList.toggle("active");
+    overlay.classList.toggle("active");
+  }
+
+  if (menuIcon) menuIcon.addEventListener("click", toggleMenu);
+  if (closeBtn) closeBtn.addEventListener("click", toggleMenu);
+  if (overlay) overlay.addEventListener("click", toggleMenu);
+
+
+
   const grid = document.getElementById("pokemonGrid");
   const search = document.getElementById("search");
   const typeFilter = document.getElementById("typeFilter");
